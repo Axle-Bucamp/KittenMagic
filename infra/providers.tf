8bb3219a7e43b3121f15659fd6f62be0ebe586cf
Bucamp Axle <47558509+Axle-Bucamp@users.noreply.github.com>
2025-03-04 01:33:18 -0600

    Create providers.tf file in /infra directory

--- infra/providers.tf
+++ infra/providers.tf
@@ -1,2 +1,19 @@
-# Provider configuration for Terraform
-# Specify your providers here
+# providers.tf
+
+provider "aws" {
+  region = var.aws_region
+}
+
+provider "vault" {
+  # Vault provider configuration
+  # This will be configured later when we set up Vault
+}
+
+# You might want to add more providers here in the future, such as:
+# provider "kubernetes" {
+#   # Kubernetes provider configuration
+# }
+
+# provider "helm" {
+#   # Helm provider configuration
+# }
