6dab32217208fbc4865c31d22acc6a202b0100fb
Bucamp Axle <47558509+Axle-Bucamp@users.noreply.github.com>
2025-03-04 01:32:41 -0600

    Create variables.tf file in /infra directory

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,2 +1,37 @@
-# Input variables for Terraform configuration
-# Define your variables here
+# variables.tf
+
+variable "aws_region" {
+  description = "The AWS region to deploy resources"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_cidrs" {
+  description = "CIDR blocks for the subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "ecs_task_cpu" {
+  description = "CPU units for the ECS task"
+  type        = number
+  default     = 256
+}
+
+variable "ecs_task_memory" {
+  description = "Memory for the ECS task"
+  type        = number
+  default     = 512
+}
+
+variable "app_count" {
+  description = "Number of Docker containers to run"
+  type        = number
+  default     = 3
+}
